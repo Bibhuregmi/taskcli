96e9b0af2c4535cba0457d04574bc410b7cd061f
Bibhu Regmi <bibhushitregmi0@gmail.com>
2025-03-02 19:20:34 -0500

    Modified the logic to handle the user error properly

--- src/main/TaskManager.java
+++ src/main/TaskManager.java
@@ -18,13 +18,15 @@ public class TaskManager {
 
     //method to remove the task
     public int removeTask(int id){
-        for(Task task : tasks) {
-            if(task.getId() == id){
-                tasks.remove(task); 
-                System.out.println("Task with id: " + id + " has been removed!" );
+        if(tasks.isEmpty()){
+            System.out.println("No any tasks were found to remove! Please add tasks before removing!");
+        }
+        for(Task task : tasks){
+            if(task.getId() != id){
+                System.out.println("No any task with id " + id + " was found! Please input correct id again");
             }else{
-                System.out.println("Invalid id!");
-
+                tasks.remove(task);
+                System.out.println("Task with id " + id + " was removed successfully!" );
             }
         }
         return id; 
@@ -36,30 +38,38 @@ public class TaskManager {
             System.out.println("No any  tasks to show");
         }else{
             for(Task task : tasks){
-                System.out.println(task);
+                System.out.println(task.toString());
             }
         }
     }
 
     //method to set completed stauts 
     public void setStatusCompleted(int id){
-        for(Task task : tasks){
-            if(task.getId() == id){
+        if(tasks.isEmpty()){
+            System.out.println("No any tasks were found to remove! Please add tasks before changing stautus!");
+        }for(Task task : tasks){
+            if(task.getId() != id){
+                System.out.println("No any task with id " + id + " was found! Please input correct id again");
+            }else{
                 task.markCompleted();
-                System.out.println("Task with id " + id + " has been completed sucessfully!" );
+                System.out.println("Task with id " + id + " is marked completed!" );
             }
         }
     }
 
     //method to set incompleted status
     public void setStautsIncomplete(int id){
-        for(Task task : tasks){
-            if(task.getId() == id){
+        if(tasks.isEmpty()){
+            System.out.println("No any tasks were found to remove! Please add tasks before changing stautus!");
+        }for(Task task : tasks){
+            if(task.getId() != id){
+                System.out.println("No any task with id " + id + " was found! Please input correct id again");
+            }else{
                 task.markIncomplete();
-                System.out.println("Task with id " + id + " is has been marked incomplete! ");
+                System.out.println("Task with id " + id + " is marked incomplete!" );
             }
         }
     }
 }
 
-// TODO: Require proper formatting of the toString() method
+// TODO: Require proper formatting of the toString() method  
